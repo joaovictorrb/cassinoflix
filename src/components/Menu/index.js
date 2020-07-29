bb6202514d28a4ca12eb087e1aae33995efcca74
joaovictorrb <joaov.rbrum@gmail.com>
2020-07-28 23:50:15 -0300

    SlickSlider added

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from "../../assets/img/logo.png";
-import "./Menu.css";
-//import ButtonLink from "./components/ButtonLink";
-import Button from "../Button";
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="logo" src={logo} alt="CassinoFlix"/>
-            </Link>
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Video
-            </Button>
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
